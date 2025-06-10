ed1cea2ea51406a8e6d26904c5f923ad8a765782
StefanNazarov76 <stefannazarov76@gmail.com>
2025-06-10 19:17:22 +0300

    add: 2418. Sort the People

---
+++ 2418. Sort the People.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        people = list(zip(heights, names))
+        people.sort(reverse=True)
+
+        return [names for heights, names in people]
