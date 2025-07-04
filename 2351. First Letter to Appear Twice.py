34ecf15b5ef5adc149db6cfa55015a00a42377c3
StefanNazarov76 <stefannazarov76@gmail.com>
2025-07-04 13:47:04 +0300

    add: 2351. First Letter to Appear Twice

---
+++ 2351. First Letter to Appear Twice.py
@@ -0,0 +1,9 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        char_set = set()
+
+        for char in s:
+            if char in char_set:
+                return char
+
+            char_set.add(char)
