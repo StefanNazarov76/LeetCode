8eaad876a765165d1f0110b2f90336d4c1329525
StefanNazarov76 <stefannazarov76@gmail.com>
2025-07-16 17:38:22 +0300

    add: 1963. Minimum Number of Swaps to Make the String Balanced

---
+++ 1963. Minimum Number of Swaps to Make the String Balanced.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        count = 0
+
+        for ch in s:
+            if ch == '[':
+                count += 1
+            else:
+                if count > 0:
+                    count -= 1
+
+        return (count + 1) // 2
