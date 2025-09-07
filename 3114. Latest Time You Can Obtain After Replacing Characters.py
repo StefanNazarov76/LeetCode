5c11f65b24b502c041e247a692a83b8f2b8e4771
StefanNazarov76 <stefannazarov76@gmail.com>
2025-09-07 16:27:07 +0300

    add: 3114. Latest Time You Can Obtain After Replacing Characters

---
+++ 3114. Latest Time You Can Obtain After Replacing Characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findLatestTime(self, s: str) -> str:
+        ans = list(s)
+
+        if s[0] == '?':
+            ans[0] = '1' if s[1] == '?' or s[1] < '2' else '0'
+
+        if s[1] == '?':
+            ans[1] = '1' if ans[0] == '1' else '9'
+
+        if s[3] == '?':
+            ans[3] = '5'
+
+        if s[4] == '?':
+            ans[4] = '9'
+
+        return ''.join(ans)
