f2778401640f82de17aded83e4d906beaf3655fb
StefanNazarov76 <stefannazarov76@gmail.com>
2025-09-23 12:43:02 +0300

    add: 165. Compare Version Numbers

---
+++ 165. Compare Version Numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1, v2 = version1.split('.'), version2.split('.')
+        n = max(len(v1), len(v2))
+
+        for i in range(n):
+            num1 = int(v1[i]) if i < len(v1) else 0
+            num2 = int(v2[i]) if i < len(v2) else 0
+
+            if num1 > num2:
+                return 1
+            elif num1 < num2:
+                return -1
+
+        return 0
