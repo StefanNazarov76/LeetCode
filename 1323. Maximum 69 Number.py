231bfbccae0421b3652cb68c3487d2bc00c9e279
StefanNazarov76 <stefannazarov76@gmail.com>
2025-09-23 12:43:27 +0300

    add: 1323. Maximum 69 Number

---
+++ 1323. Maximum 69 Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        ans = str(num)
+
+        for i, d in enumerate(ans):
+            if d == '6':
+                return int(ans[:i] + '9' + ans[i + 1:])
+
+        return num
