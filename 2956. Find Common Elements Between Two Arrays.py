f71798f2992843322de6e62ea4a65aaa56029dde
StefanNazarov76 <stefannazarov76@gmail.com>
2025-10-03 19:32:09 +0300

    add: 2956. Find Common Elements Between Two Arrays

---
+++ 2956. Find Common Elements Between Two Arrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        cnt1 = sum(1 for x in nums1 if x in set2)
+        cnt2 = sum(1 for x in nums2 if x in set1)
+
+        return [cnt1, cnt2]
