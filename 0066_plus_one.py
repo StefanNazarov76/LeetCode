75cdaa8d3b00e89369514a5a48c29139c4003a9f
StefanNazarov76 <stefannazarov76@gmail.com>
2026-01-01 21:33:25 +0200

    add: 66. Plus One

---
+++ 0066_plus_one.py
@@ -0,0 +1,17 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = len(digits) - 1
+
+        while True:
+            if digits[i] == 9:
+                digits[i] = 0
+                i -= 1
+
+                if i < 0:
+                    digits.insert(0, 1)
+                    break
+            else:
+                digits[i] += 1
+                break
+
+        return digits
