3b4ea5571578e2006ec5198a47a8c8b087efbc0d
StefanNazarov76 <stefannazarov76@gmail.com>
2026-01-21 18:31:23 +0200

    add: 1844. Replace All Digits with Characters

---
+++ 1844_replace_all_digits_with_characters.py
@@ -0,0 +1,11 @@
+class Solution:
+    def replaceDigits(self, s: str) -> str:
+        ans = ''
+
+        for i in range(len(s)):
+            if i % 2 != 0:
+                ans += chr(ord(s[i - 1]) + int(s[i]))
+            else:
+                ans += s[i]
+
+        return ans
