cacdab6f7eb580c3f5d9574b8be7c69a1e2901ec
StefanNazarov76 <stefannazarov76@gmail.com>
2026-01-26 13:02:56 +0200

    add: 1200. Minimum Absolute Difference

---
+++ 1200_minimum_absolute_difference.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        min_diff = float('inf')
+        ans = []
+
+        for i in range(len(arr) - 1):
+            diff = abs(arr[i + 1] - arr[i])
+
+            if diff < min_diff:
+                min_diff = diff
+                ans = [[arr[i], arr[i + 1]]]
+
+            elif diff == min_diff:
+                ans.append([arr[i], arr[i + 1]])
+
+        return ans
