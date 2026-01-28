8cc54f90e3c051030a2143602ef8464f57d6f89e
StefanNazarov76 <stefannazarov76@gmail.com>
2026-01-28 18:06:09 +0200

    add: 2697. Lexicographically Smallest Palindrome

---
+++ 2697_lexicographically_smallest_palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def makeSmallestPalindrome(self, s: str) -> str:
+        left, right = 0, len(s) - 1
+        s = list(s)
+
+        while left < right:
+            min_char = min(s[left], s[right])
+            s[left] = s[right] = min_char
+
+            left += 1
+            right -= 1
+
+        return ''.join(s)
