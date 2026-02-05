d60f7bacb3b620967b39f438bf2b80d9d82d905f
StefanNazarov76 <stefannazarov76@gmail.com>
2026-02-05 20:25:19 +0200

    add: 657. Robot Return to Origin

---
+++ 0657_robot_return_to_origin.py
@@ -0,0 +1,15 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x, y = 0, 0
+
+        for m in moves:
+            if m == 'L':
+                x -= 1
+            elif m == 'R':
+                x += 1
+            elif m == 'U':
+                y += 1
+            else:
+                y -= 1
+
+        return x == 0 and y == 0
