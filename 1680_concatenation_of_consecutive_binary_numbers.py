6b9a0903a0303c4a7ce1a93db0a47e011c872fb4
StefanNazarov76 <stefannazarov76@gmail.com>
2026-02-28 18:44:13 +0200

    add: 1680. Concatenation of Consecutive Binary Numbers

---
+++ 1680_concatenation_of_consecutive_binary_numbers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        ans = ''
+
+        for i in range(1, n + 1):
+            ans += bin(i)[2:]
+
+        return int(ans, 2) % (10**9 + 7)
