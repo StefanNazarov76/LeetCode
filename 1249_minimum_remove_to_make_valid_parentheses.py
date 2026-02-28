337ddcee3f22a94abf9d05fb7bec647ea2a8c295
StefanNazarov76 <stefannazarov76@gmail.com>
2026-02-28 18:45:48 +0200

    add: 1249. Minimum Remove to Make Valid Parentheses

---
+++ 1249_minimum_remove_to_make_valid_parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        remove = set()
+
+        for i, c in enumerate(s):
+            if c == "(":
+                stack.append(i)
+            elif c == ")":
+                if stack:
+                    stack.pop()
+                else:
+                    remove.add(i)
+
+        remove.update(stack)
+        return ''.join(s[i] for i in range(len(s)) if i not in remove)
