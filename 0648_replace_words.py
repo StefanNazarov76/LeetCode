322edfefeae61a6055441937ff37e96c20cae906
StefanNazarov76 <stefannazarov76@gmail.com>
2026-03-06 22:28:25 +0200

    add: 648. Replace Words

---
+++ 0648_replace_words.py
@@ -0,0 +1,10 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        sentence = sentence.split()
+
+        for i in range(len(sentence)):
+            for w in dictionary:
+                if sentence[i].startswith(w):
+                    sentence[i] = w
+
+        return ' '.join(sentence)
