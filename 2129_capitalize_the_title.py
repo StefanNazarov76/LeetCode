9b72f501f84b17f2f5b73964d3b31b36686d3661
StefanNazarov76 <stefannazarov76@gmail.com>
2026-03-08 15:01:50 +0200

    add: 2129. Capitalize the Title

---
+++ 2129_capitalize_the_title.py
@@ -0,0 +1,11 @@
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        ans = []
+
+        for w in title.split():
+            if len(w) <= 2:
+                ans.append(w.lower())
+            else:
+                ans.append(w.capitalize())
+
+        return ' '.join(ans)
